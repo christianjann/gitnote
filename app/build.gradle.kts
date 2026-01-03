85449c75ce1dab67c059016a5590234359709d90
Christian Jann <christian.jann@ymail.com>
2026-01-03 22:11:04 +0100

    Bump after release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "io.github.christianjann.gitnotecje"
         minSdk = 30
         targetSdk = 36
-        versionCode = 18
-        versionName = "26.01.3"
+        versionCode = 19
+        versionName = "26.01.4"
 
         buildConfigField(
             "String",
